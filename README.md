d87c61db722c33d78441208200f1da754dad16e8
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-07-10 02:05:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Philomena-Booru-Switcher
+
+Requires [Violentmonkey](https://violentmonkey.github.io/) or compatible userscript manager.  
+[Click here to install](https://github.com/marktaiwan/Philomena-Booru-Switcher/raw/master/booru-switcher.user.js)
+
+## Screenshot
+
+![Screenshot](https://raw.githubusercontent.com/marktaiwan/Philomena-Booru-Switcher/master/screenshots/screenshot.png)
---
+++ booru-switcher.user.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         Booru Switcher
+// @description  Switch between Philomena-based boorus
+// @version      1.0.0
+// @author       Marker
+// @license      MIT
+// @namespace    https://github.com/marktaiwan/
+// @homepageURL  https://github.com/marktaiwan/Philomena-Booru-Switcher
+// @supportURL   https://github.com/marktaiwan/Philomena-Booru-Switcher/issues
+// @match        *://*.derpibooru.org/*
+// @match        *://*.trixiebooru.org/*
+// @match        *://*.ponybooru.org/*
+// @match        *://*.ponerpics.org/*
+// @inject-into  content
+// @noframes
+// ==/UserScript==
+(function () {
+
+const SCRIPT_ID = 'booru_switcher';
+const boorus = [
+  {name: 'Ponybooru', host: 'ponybooru.org'},
+  {name: 'Ponerpics', host: 'ponerpics.org'},
+  {name: 'Derpibooru', host: 'derpibooru.org'},
+  {name: 'Trixiebooru', host: 'trixiebooru.org'},
+];
+
+function $(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function initUI() {
+  const header = $('header.header');
+  const headerRight = $('.header__force-right', header);
+  const menuButton = document.createElement('div');
+  menuButton.classList.add('dropdown', 'header__dropdown', `${SCRIPT_ID}__menu`);
+  menuButton.innerHTML = `
+<a class="header__link" href="#" data-click-preventdefault="true" title="Switch booru">
+  <i class="${SCRIPT_ID}__icon fa fa-list-ul"></i>
+  <span class="hide-limited-desktop hide-mobile">Switch</span>
+  <span data-click-preventdefault="true"><i class="fa fa-caret-down"></i></span>
+</a>
+<nav class="dropdown__content"></nav>`;
+
+  const nav = $('nav', menuButton);
+
+  for (const booru of boorus) {
+    const {name, host} = booru;
+    if (window.location.host.match(host)) continue;
+    const anchor = document.createElement('a');
+    anchor.classList.add('header__link');
+    anchor.href = '#';
+    anchor.dataset.host = host;
+    anchor.innerText = name;
+
+    nav.append(anchor);
+  }
+
+  nav.addEventListener('click', e => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (!e.target.matches('a')) return;
+    window.location.href = window.location.href.replace(window.location.host, e.target.dataset.host);
+  });
+
+  headerRight.insertAdjacentElement('beforebegin', menuButton);
+}
+
+if ($('#image_target')
+  || window.location.pathname.startsWith('/forums')
+  || window.location.pathname.startsWith('/images/')) {
+  return;
+}
+
+initUI();
+})();
