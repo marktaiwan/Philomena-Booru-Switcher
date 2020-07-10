0f1003aef5090e4b99f4043eefdb5db3b1377217
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-07-10 19:24:12 +0800

    Reset pagination

--- booru-switcher.user.js
+++ booru-switcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booru Switcher
 // @description  Switch between Philomena-based boorus
-// @version      1.0.1
+// @version      1.0.2
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
@@ -28,6 +28,25 @@ function $(selector, parent = document) {
   return parent.querySelector(selector);
 }
 
+function getQueryVariables() {
+  const search = window.location.search;
+  if (search === '') return {};
+
+  const arr = window.location.search
+    .substring(1)
+    .split('&')
+    .map(string => {
+      return [key, val] = string.split('=');
+    });
+
+  const dict = {};
+  for (const list of arr) {
+    dict[list[0]] = list[1];
+  }
+
+  return dict;
+}
+
 function initUI() {
   const header = $('header.header');
   const headerRight = $('.header__force-right', header);
@@ -43,12 +62,20 @@ function initUI() {
 
   const nav = $('nav', menuButton);
 
+  const searchDict = getQueryVariables();
+  if (searchDict && searchDict.page) {
+    delete searchDict.page;
+  }
+  const searchStr = (Object.keys(searchDict).length)
+    ? '?' + Object.entries(searchDict).map(arr => arr.join('=')).join('&')
+    : '';
+
   for (const booru of boorus) {
     const {name, host} = booru;
     if (window.location.host.match(host)) continue;
     const anchor = document.createElement('a');
     anchor.classList.add('header__link');
-    anchor.href = window.location.href.replace(window.location.host, host);
+    anchor.href = window.location.protocol + '//' + host + window.location.pathname + searchStr;
     anchor.innerText = name;
 
     nav.append(anchor);
