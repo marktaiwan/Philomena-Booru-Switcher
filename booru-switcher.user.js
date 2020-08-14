36bb9ad06109a7560ddc0f290ec00fddca0e5b50
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-08-14 21:05:21 +0800

    Version bump 1.2.3

--- booru-switcher.user.js
+++ booru-switcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booru Switcher
 // @description  Switch between Philomena-based boorus
-// @version      1.2.2
+// @version      1.2.3
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
