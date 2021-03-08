09155323f9de0745e67d282a64fbcce0f326711f
Mark Taiwan <marktaiwan@users.noreply.github.com>
2021-03-08 13:20:02 +0800

    Don't run on Twibooru pastes

--- booru-switcher.user.js
+++ booru-switcher.user.js
@@ -456,7 +456,13 @@ function log(obj) {
 
 if ($('#image_target') || $('#thumbnails-not-yet-generated')) {
   initSearchUI();
-} else if (!window.location.pathname.match(/^\/forums\/\w+\/topics/)) {
+} else {
+  // forum pages
+  if (!window.location.pathname.match(/^\/forums\/\w+\/topics/)) return;
+
+  // Twibooru pastes
+  if ($('ol.paste-content')) return;
+
   initSwitcherUI();
 }
 
